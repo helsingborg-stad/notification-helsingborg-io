e9373f96c405ae01e270c31f040be4c92c59e62e
morganottosson <morganottosson@outlook.com>
2018-12-03 14:35:12 +0100

    updated to latest structure

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
+'use strict';
 const express = require('express');
 const https = require('https');
-const fs = require('fs');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger/swagger.json');
+const fs = require('fs');
+require('body-parser-xml')(bodyParser);
 
 const app = express();
-const port = 3000;
 
 // !!!!!!!!!!!!!!!!!*********** Dont have this in prod
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.xml({ normalize: true }));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.use(require('./components'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert'),
+const server = https.createServer({
+    cert: fs.readFileSync(process.env.SERVERCERT),
+    key: fs.readFileSync(process.env.SERVERKEY),
     requestCert: true,
     rejectUnauthorized: false
-}, app).listen(port, () => console.log(`Example app listening on port ${port}!`));
+}, app).listen(process.env.PORT, () => console.log(`Example app listening on port 3000!`));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+module.exports = server;
---
+++ components/person/dal.js
@@ -0,0 +1,77 @@
+const redis = require('redis');
+const axios = require('axios');
+const https = require('https');
+const client = redis.createClient();
+const fs = require('fs');
+const navetData = require('./objectSchemas/navetData');
+
+client.on('error', (err) => {
+    console.log('Error ' + err);
+});
+
+const axiosClient = axios.create({
+    httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+        key: fs.readFileSync(process.env.komAKey),
+        cert: fs.readFileSync(process.env.komACrt),
+        passphrase: process.env.passphrase
+    }),
+    headers: {
+        'Content-Type': 'text/xml;charset=UTF-8'
+    }
+});
+
+exports.getPerson = async (body) => {
+    try {
+        console.log('did i enter');
+        const id = body.id;
+        let reply = await hget(id);
+        /*
+        Example of an axios call with https and certificates
+        and validating the response
+        const res = await axiosClient.post(process.env.navetUrl, { id });
+        res.data.id = res.data.Folkbokforingspost.Personpost.PersonId.PersonNr;
+        const validResNavet = await validate(res.data, navetData);
+        */
+        return reply;
+    } catch (error) {
+        return error;
+    }
+};
+
+// validating with Joi
+const validate = (input, schema) => {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(schema.validate(input));
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+// getting the object with key id
+const hget = (id) => {
+    return new Promise((resolve, reject) => {
+        client.hget('persons', id, (err, reply) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(reply);
+            }
+        });
+    });
+};
+
+// saving in redis
+const hset = (id, stringData) => {
+    return new Promise((resolve, reject) => {
+        client.hmset('persons', id, stringData, (err, reply) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(reply);
+            }
+        });
+    });
+};
