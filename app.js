29ba93b310222c9a775b40422fb4dce151e9af2d
Samatar Mahamed <samatar.mahamed@softhouse.se>
2019-05-07 14:17:22 +0200

    Added Config files and updated app file on change

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const https = require('https');
+const config = require('config');
 const swaggerUi = require('swagger-ui-express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const swaggerDocument = require('./swagger/swagger.json');
 
 const app = express();
+/**
+ * Init Config
+ */
+const SERVER_PORT = config.get('SERVER.PORT');
+const SERVER_KEY = config.get('SERVER.KEY');
+const SERVER_CERT = config.get('SERVER.CERT');
+
 
 // !!!!!!!!!!!!!!!!!*********** Dont have this in prod
 // if not set to 0 it will reject connections with self signed certificates
@@ -25,14 +33,15 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Create server with https and cert/key configured.
 const server = https
   .createServer({
-    key: fs.readFileSync(process.env.SERVER_KEY),
-    cert: fs.readFileSync(process.env.SERVER_CERT),
+    key: fs.readFileSync(SERVER_KEY),
+    cert: fs.readFileSync(SERVER_CERT),
     requestCert: true,
     rejectUnauthorized: false,
   }, app)
+
 // Listen on port specfied in env-file.
-  .listen(process.env.PORT,
-    () => console.log(`Example app listening on port ${process.env.PORT}!`)); // eslint-disable-line no-console
+  .listen(SERVER_PORT,
+    () => console.log(`Example app listening on port ${SERVER_PORT}!`)); // eslint-disable-line no-console
 
 // Export server to use it in tests.
 module.exports = server;
