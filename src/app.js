6e483032a8e4c21606e46d574eae41e7f4b9f252
Rasmus Letterkrantz <rasmus.letterkrantz@gmail.com>
2019-05-21 09:25:21 +0200

    Removed https from the express server; we want https to 1. be optional (can be a non-exposed module) and 2. be handled by some separate entity (nginx, kubernetes etc)

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const https = require('https');
 const config = require('config');
 const swaggerUi = require('swagger-ui-express');
-const fs = require('fs');
 const bodyParser = require('body-parser');
 const swaggerDocument = require('../swagger/swagger.json');
 const routes = require('./components/routes');
@@ -12,13 +10,6 @@ const app = express();
  * Init Config
  */
 const SERVER_PORT = config.get('SERVER.PORT');
-const SERVER_KEY = config.get('SERVER.KEY');
-const SERVER_CERT = config.get('SERVER.CERT');
-
-
-// !!!!!!!!!!!!!!!!!*********** Dont have this in prod
-// if not set to 0 it will reject connections with self signed certificates
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -31,16 +22,8 @@ app.use(routes());
 // Swagger for documenting the api, access through localhost:xxxx/api-docs.
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Create server with https and cert/key configured.
-const server = https.createServer({
-  key: fs.readFileSync(SERVER_KEY),
-  cert: fs.readFileSync(SERVER_CERT),
-  requestCert: true,
-  rejectUnauthorized: false,
-}, app);
-
 // Listen on port specfied in env-file.
-server.listen(SERVER_PORT,
+const server = app.listen(SERVER_PORT,
   () => console.log(`Example app listening on port ${SERVER_PORT}!`)); // eslint-disable-line no-console
 
 // Export server to use it in tests.
